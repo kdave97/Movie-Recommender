b2a146d5ba1e7ee246682bbdaa008022a8f78f2f
karan <karandave1997@gmail.com>
2020-07-07 19:48:18 -0400

    Updated file

--- FLASK/content_model.py
+++ FLASK/content_model.py
@@ -11,6 +11,9 @@ movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
 movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '')
 movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
 movies_df['genres'] = movies_df.genres.str.split('|')
+movies_df=movies_df[movies_df['year'].notna()]
+movies_df['year']=movies_df['year'].astype('int')
+movies_df=movies_df.loc[movies_df.year>1980]
 moviesWithGenres_df = movies_df.copy()
 
 
@@ -32,6 +35,7 @@ def recommend(userInput):
 	userGenreTable = userMovies.drop('movieId', 1).drop('title', 1).drop('genres', 1).drop('year', 1)
 	userProfile = userGenreTable.transpose().dot(inputMovies['rating'])
 	genreTable = moviesWithGenres_df.set_index(moviesWithGenres_df['movieId'])
+	genreTable=genreTable.loc[~genreTable.movieId.isin(userMovies.movieId)]
 	genreTable = genreTable.drop('movieId', 1).drop('title', 1).drop('genres', 1).drop('year', 1)
 	recommendationTable_df = ((genreTable*userProfile).sum(axis=1))/(userProfile.sum())
 	recommendationTable_df = recommendationTable_df.sort_values(ascending=False)
